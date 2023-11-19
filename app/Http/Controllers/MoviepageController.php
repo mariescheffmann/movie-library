32074f6b83ee4355daeb52d1d6b1ed139cb98869
mariescheffmann <mariemadsen26@yahoo.dk>
2023-11-19 17:26:55 +0100

    changed classname of MoviepageController back after it was accidentally renamed

--- app/Http/Controllers/MoviepageController.php
+++ app/Http/Controllers/MoviepageController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Movie;
 
-class ActorpageController extends Controller
+class MoviepageController extends Controller
 {
     public function show($id)
     {
