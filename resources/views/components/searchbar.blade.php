23e375557e8207c1a0348650859b39adf1574d5d
Philip Ruhnau <philip.reumert.ruhnau@gmail.com>
2023-12-13 11:27:52 +0100

    Searchbar centered & made prettier

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,7 +1,7 @@
 <div>
     <form action="{{route('searchMovies') }}" method="GET">
-        <input type="text" name="movieName" placeholder="Search for movies">
-        <button type="submit">Search</button>
+        <input type="text" name="movieName" placeholder="Search for movies" class="w-96">
+        <button type="submit"><b class="text-white ml-2">Search</b></button>
     </form>
 
 
