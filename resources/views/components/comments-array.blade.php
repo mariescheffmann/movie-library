1188acf3f79097d95666fc816c8bf883d343d768
louip22 <louip22@student.sdu.dk>
2023-12-13 20:05:00 +0100

    update comments functionality complete

--- resources/views/components/comments-array.blade.php
+++ resources/views/components/comments-array.blade.php
@@ -8,7 +8,7 @@
         <div class = "p-6 box-border h-auto w-auto p-4 border-2 bg-gray-200 rounded">
             <p class = "text-gray-700 text-xl">{{ $comment->comment }}</p>
         </div>
-        <div class="flex"> <!-- Button container -->
+        <div class="flex" id="buttonContainer"> <!-- Button container -->
             <div> <!-- activate update functionality button - NOT IMPLEMENTED -->
                 <button id="updateButton" class="!bg-purple-theme hover:!bg-purple-darker-theme text-white font-bold py-2 px-4 rounded mt-2 mb-2 mr-4">Update</button>
             </div>
@@ -21,14 +21,28 @@
             </div>
             
         </div>
-        <div> <!-- update functionality -->
+        <div id="updateCommentForm" class="hidden w-auto"> <!-- update functionality -->
             <form method="POST" action="{{ route('commenthistory.update', $comment['id']) }}">
                 @method('PUT')
                 @csrf
-                <input name="comment" type="text" id="newComment" class="p-6 box-border h-auto w-auto p-4 border-2 bg-gray-200 rounded" value="{{ $comment->newComment }}">
-                <input type="submit" value= "Update" class="!bg-purple-theme hover:!bg-purple-darker-theme text-white font-bold py-2 px-4 rounded mt-2 mb-2 ml-2">
+                <input id="formText" name="comment" type="text" id="newComment" class="!p-2 !box-border !h-auto !w-4/5 !p-4 !border-2 !bg-gray-200 !rounded !text-gray-700 !text-xl !mt-2">
+                <input type="submit" value= "Update" id="updateSubmit" class="!bg-purple-theme hover:!bg-purple-darker-theme text-white font-bold py-2 px-4 rounded mt-2 mb-2 ml-2">
             </form> 
         </div>
+
+        <script>
+            document.getElementById("updateButton").addEventListener("click", updateComment);
+            function updateComment(){
+                document.getElementById("updateCommentForm").style="display:block";
+                document.getElementById("buttonContainer").style="display:none";
+            }
+            document.getElementById("updateSubmit").addEventListener("click", endUpdate);
+            function endUpdate(){
+                document.getElementById("updateCommentForm").style="display:none";
+                document.getElementById("buttonContainer").style="dispaly:block";
+            }
+        </script>
+
     </div>
     @endforeach
     </div>
