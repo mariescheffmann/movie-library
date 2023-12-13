4dd25fa63f4dbddf2d397bfcae7dc2e90dc249c4
Philip Ruhnau <philip.reumert.ruhnau@gmail.com>
2023-12-13 12:27:29 +0100

    Search functionality improved

--- resources/views/searchpage.blade.php
+++ resources/views/searchpage.blade.php
@@ -1,15 +1,30 @@
 <x-app-layout>
 
-    <form action="{{route('searchMovies') }}" method="GET">
-        <input type="text" name="movieName" placeholder="Search for movies">
-        <button type="submit">Search</button>
-    </form>
+    <h1 class="ml-4 mt-4 text-white text-3xl">Search results:<h1>
+            <div class="flex flex-wrap mt-4 ml-4">
 
-    @foreach ($searchedMovies as $movie)
-    <div>
-        <h2>{{$movie->title}}</h2>
-        <p>{{$movie->descriptionShort}}</p>
-    </div>
-    @endforeach
+                @if(($searchedMovies)->isEmpty())
+                <p class="ml-4 mt-4 text-white text-3xl">No Results</p>
+
+
+            </div>
+            @else
+
+            @foreach ($searchedMovies as $movie)
+            <div class="ml-6">
+                <div style="mr-4 float:left; width: 100%; height: 100%;">
+                    <img class="flex object-cover h-72 min-w-full" src="{{ asset('pictures/' . $movie['imageReference']) }}" alt="movie poster">
+                    <div class="mb-6">
+                        <h2 class="text-white">{{$movie->title}}</h2>
+                        <p class="text-white">{{$movie->descriptionShort}}</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+
+            @endif
+
+
 
 </x-app-layout>
