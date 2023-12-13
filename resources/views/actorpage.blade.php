e73ac1623331f884c52543f8731a9041d25e1529
mariescheffmann <mariemadsen26@yahoo.dk>
2023-12-13 12:43:48 +0100

    moviepage and actorpage cleanup, add rating to moviepage, actor bug fixed

--- resources/views/actorpage.blade.php
+++ resources/views/actorpage.blade.php
@@ -10,48 +10,32 @@ $decodedActor = json_decode($actor);
         <div style="float:left; width: 50%" class="text-gray-200 dark:text-gray-200 pt-10 pl-20 text-2xl">
             <section id="actorInfo" style="margin-top: 15px;">
                 <div style="float:left; width: 50%; height: 100%;">
-                <img class="flex object-cover h-72 min-w-full" src="{{ asset('pictures/' . $actor['imageReference']) }}" alt="movie poster">
+                    <img class="flex object-cover h-72 min-w-full" src="{{ asset('pictures/' . $decodedActor->{'imageReference'}) }}">
                 </div>
-                <div >
-                    
-                    <div style="font-size:1.25em;font-weight:bold;">
-                    <?php
-                        echo $decodedActor->{'name'};
-                    ?>
-                    </div>
-                    <div>
-                    <?php
-                        echo $decodedActor->{'birthday'}
-                    ?>
-                    </div>
-                    <div><?php
-                        echo 'nationality';
-                    ?></div>
             </section>
             <section id="biography" style="margin-top: 15px;">
-                <h1 style="font-size:1.25em;font-weight:bold;">Biography</h1>
-                <div> <?php
-                        echo $decodedActor->{'biography'}
-                    ?> </div>
+                <h1 style="font-size:1.25em;font-weight:bold; margin-top: 15px;"> {{ $decodedActor->{'name'}; }}</h1>
+                <div> {{$decodedActor->{'birthday'}; }} </div>
+                <div> {{$decodedActor->{'nationality'}; }} </div>
+                <h1 style="font-size:1.25em;font-weight:bold; margin-top: 15px;">Biography</h1>
+                <div> {{ $decodedActor->{'biography'}; }} </div>
             </section>
         </div>
-        <div class="vl; text-gray-200 dark:text-gray-200 pt-10 pl-20 text-2xl">
-            <?php
-                foreach ($movies[1] as &$value) {
-                    echo '<div class="my_class" style="margin-top: 15px;">';
-
-
-                    echo '<div class="my_class" style="float:left; width: 50%"> ';
-                    echo '<img style="float:left; margin-bottom:15px;" src="buttonpng" width="120px" height="120px"/>'; 
-                    echo '<div style="font-weight:bold;">' .  $value["title"] . '</div>';
-                    echo '<div>' . 'Duration: ' . $value["duration"]. '</div>';
-                    echo '<div>' . 'Release year: ' . $value["releaseYear"]. '</div>';
-                    echo '<div>' . $value["descriptionShort"]. '</div>';
-                    echo '</div>';
+        <div style="float: right; width: 50%" class="vl; text-gray-200 dark:text-gray-200 pt-10 pl-20 text-2xl">
+             <div style="margin-top: 15px; width: 100%"> {{ $decodedActor->{'name'}; }} appears in:
+                <?php foreach ($movies[0] as &$value): ?>
+                    <div style="margin-top: 15px; width: 100%;">
+                        <div style="float:left; width: 100%;">
+                            <img style="float:left; margin-bottom:15px;" width="120px" height="120px" src="{{ asset('pictures/' . $value['imageReference']) }}"/>
+                            <div style="font-weight:bold;">{{ $value["title"]; }}</div>
+                            <div>Duration: {{ $value["duration"]; }}</div>
+                            <div>Release year: {{ $value["releaseYear"];}}</div>
+                            <div>{{ $value["descriptionShort"];}}</div>
+                        </div>
                     
-                    echo '</div>';
-                }
-            ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
     </body>
 </html>
